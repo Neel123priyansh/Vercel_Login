8640db4ddb45f30d44b25296d49aa3dddcd4b752
Priyansh Neel <neelpriyansh@gmail.com>
2025-03-25 15:02:20 +0530

    backend corrcection

--- backend/index.js
+++ backend/index.js
@@ -65,4 +65,13 @@ app.post("/reg", (req, res) => {
   return "Heelo, its working";
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://vercel-login-frontend.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+
 app.listen(3000, () => console.log("Server running on port 3000"));
